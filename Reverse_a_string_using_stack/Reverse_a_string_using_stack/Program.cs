20ef97d440a5c31e8c41c38c879cac8be2be17fb
Ananth <antz5@users.noreply.github.com>
2016-12-06 11:35:54 +0530

    Algorithm to reverse a string using stack data structure

---
+++ Reverse_a_string_using_stack/Reverse_a_string_using_stack/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_string_using_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                 [X]  1) Create an empty stack.
+                 [X]  2) One by one push all characters of string to stack.
+                 [X]  3) One by one pop all characters from stack and put them back to string.
+            */
+
+            string s = Console.ReadLine();
+            Stack stack = new Stack(s.Length);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                stack.Push(s[i]);
+            }
+
+            //Reverse logic - pop all chars and put them back to string s
+            s = "";
+            while (stack.top >= 0)
+            {
+                s += stack.Pop();
+            }
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+   
+}
+
+public class Stack
+{
+    public int top = -1;
+    char[] array;
+
+    public bool isStackEmpty()
+    {
+        return top == -1;
+    }
+
+    public bool isStackFull()
+    {
+        return top == array.Length - 1;
+    }
+    public Stack(int capacity)
+    {
+        array = new char[capacity];
+    }
+
+    public void Push(char data)
+    {
+        if (!isStackFull())
+        {
+            array[++top] = data;
+        }
+    }
+
+    public char Pop()
+    {        
+        return array[top--];
+    }
+}
---
+++ Reverse_a_string_using_stack/Reverse_a_string_using_stack/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Reverse_a_string_using_stack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Reverse_a_string_using_stack")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0dbc57cf-35ad-480a-b5b4-e01e099ee56e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
