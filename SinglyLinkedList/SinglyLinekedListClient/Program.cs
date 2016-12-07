aca0ba7d439552a5228aae945125f3a0f0434cf7
Ananth <antz5@users.noreply.github.com>
2016-12-07 19:19:02 +0530

    Added a Singly Linked list with the following functionalities  [X] create a Node class with a constructor which initializes the data and next         [X] create a method to add node to the end of list         [X] create a method to add node to the beginning of the list         [X] create a method to print all the nodes in the list         [X] create a property that returns count/or find the length of the list         [X] create a method to delete a given key         [X] create a method to add node after a given key         [X] create a method to add node before a given key         [X] create a method to return the index of the node based on the given key         [X] create a method to clear the list         [X] create a method to reverse the list Also, there is a client to test the above methods which is a console application.

---
+++ SinglyLinkedList/SinglyLinekedListClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SinglyLinkedList;
+namespace SinglyLinekedListClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.AddToBeginning(5);
+            list.AddToBeginning(10);
+            list.AddToBeginning(15);
+            list.AddToBeginning(20);
+            list.AddToBeginning(25);
+
+            list.AddToEnd(30);
+            list.AddToEnd(35);
+            list.AddToEnd(40);
+            list.AddToEnd(45);
+            list.AddToEnd(50);
+
+            Console.WriteLine("The size of the singly linked list is : " + list.Count);
+            Console.WriteLine();
+            list.DisplayList();
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Enter the key for the node to be removed");
+            int key = int.Parse(Console.ReadLine());
+
+            int removed = list.Remove(key);
+            if (removed == -1)
+                Console.WriteLine("Key not found");
+            else
+                Console.WriteLine("The key: " + removed + " has been removed from the list");
+
+            Console.WriteLine("The updated list is ");
+
+            list.DisplayList();
+            Console.WriteLine("Enter the key where a new data has to be inserted after the key node");
+            int keyAfter = int.Parse(Console.ReadLine());
+
+            list.InsertAfter(keyAfter, 100);
+
+            Console.WriteLine("The updated list is ");
+            list.DisplayList();
+
+            Console.WriteLine("Enter a key to get the index of the node");
+            int keyFind = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Key found at the " + list.Find(keyFind) + " position");
+
+            list.DisplayList();
+            Console.WriteLine("Enter the key where a new data has to be inserted before the key node");
+            int keyBefore = int.Parse(Console.ReadLine());
+            list.InsertBefore(keyBefore, 200);
+            list.DisplayList();
+            list.Clear();
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            list.Reverse();
+            list.DisplayList();
+            Console.WriteLine("The size of the list is " + list.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SinglyLinkedList/SinglyLinkedList/LinkedList.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    /*
+        [X] create a Node class with a constructor which initializes the data and next
+        [X] create a method to add node to the end of list
+        [X] create a method to add node to the beginning of the list
+        [X] create a method to print all the nodes in the list
+        [X] create a property that returns count/or find the length of the list
+        [X] create a method to delete a given key        
+        [X] create a method to add node after a given key
+        [X] create a method to add node before a given key        
+        [X] create a method to return the index of the node based on the given key                
+        [X] create a method to clear the list       
+        [X] create a method to reverse the list        
+
+    */
+
+    public class Node
+    {
+        internal int data { get; set; }
+        internal Node next { get; set; }        
+
+        internal Node(int data)
+        {
+           this.data = data;
+            this.next = null;
+        }
+
+        internal void AddToEnd(int data)
+        {
+            if (next == null)
+            {
+                next = new Node(data); // if list is empty then create a new node
+            }
+            else
+            {
+                next.AddToEnd(data); //recursive call to add the new node at the end
+            }
+        }
+        
+        internal void PrintList()
+        {
+            Console.Write("|"+data+"|->"); //Print the nodes in the given string format
+            if (next != null)
+            {
+                next.PrintList(); //Recursive call to print all nodes until next is null
+            }
+        }
+
+        private int SizeOfList()
+        {
+            int count = 0;
+            if (next != null)
+            {
+                while (next != null)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+
+    public class LinkedList
+    {
+        Node head;
+        public int Count;
+        public LinkedList()
+        {
+            head = null;
+            Count = 0;
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+                Count += 1;
+            }
+            else
+            {
+                head.AddToEnd(data);
+                Count += 1;
+            }
+        }
+
+        public void AddToBeginning(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data); // if list is empty then create a new node
+                Count += 1;
+            }
+            else
+            {
+                Node temp = new Node(data); //if the list is not empty then create a temporary node and point to the current node
+                temp.next = head;
+                head = temp;
+                Count += 1;
+            }
+        }
+
+        public void DisplayList()
+        {
+            if (head != null)
+            {
+                head.PrintList();
+            }
+            else
+            {
+                Console.WriteLine("The List Contains no data");
+            }
+        }
+
+        public int Remove(int key)
+        {
+            int removedData = -1;
+
+            Node current = head;
+            Node previous = null;
+
+            if (current != null && current.data == key)
+            {
+                removedData = current.data;
+                head = current.next;
+                Count -= 1;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    if (current.data != key)
+                    {
+                        previous = current;
+                        current = current.next;
+                    }
+                    else
+                    {
+                        removedData = current.data;
+                        current = current.next;
+                        previous.next = current;
+                        Count -= 1;                                            
+                    }
+                }
+            }
+            return removedData;
+        }
+
+        public int Find(int key)
+        {
+            int index = -1;
+            Node current = head;
+            while (current != null)
+            {
+                index += 1;
+                if (current.data != key)
+                {
+                    current = current.next;                    
+                }
+                else
+                {
+                    return index;
+                }            
+            }
+            return index;
+        }
+
+        public void InsertAfter(int key, int data)
+        {
+            Node current = head;
+            Node previous = null;
+
+            if (head == null)
+            {
+                head = new Node(data);
+                Count += 1;
+            }
+            else
+            {
+                while (current != null)
+                {
+                    if (current.data != key)
+                    {
+                        previous = current;
+                        current = current.next;
+                    }
+                    else
+                    {
+                        Node node = new Node(data);
+                        node.next = current.next;
+                        current.next = node;
+                        current = current.next;
+                        Count += 1;
+                    }
+                }
+            }
+        }
+
+        public void InsertBefore(int key, int data)
+        {
+            Node current = head;
+            Node previous = head;            
+
+            if (head == null)
+            {
+                head = new Node(data);
+                Count += 1;
+            }
+            else
+            {               
+                while (current != null)
+                {                    
+                    if (current.data != key)
+                    {
+                        previous = current;
+                        current = current.next;
+                    }
+                    else
+                    {
+                        Node node = new Node(data);
+                        node.next = current;
+                        previous.next = node;
+                        current = current.next;
+                        Count += 1;
+                    }
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            head = null;
+            Count = 0;
+        }
+
+        public void Reverse()
+        {
+            Node current = head;
+            Node previous = null;
+            Node next = null;
+           
+            while (current != null)
+            {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;                  
+            }
+            head = previous;
+        }
+    }
+}
