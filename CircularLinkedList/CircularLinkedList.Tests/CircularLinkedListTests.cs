c9c5210b9ace6b7b4f7c28e94f03ca154ce5eb7c
Ananth <antz5@users.noreply.github.com>
2016-12-09 18:22:56 +0530

    Circular Linked List with the following operations   [X] create a method to add a node to the list         [X] create a method to display the contents of the list         [X] create a method to Add to beginning of the list         [X] create a property to return the number of nodes in the list         [X] create a method to Remove a node based on the given key

---
+++ CircularLinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircularLinkedList.Tests
+{
+    [TestClass]
+    public class CircularLinkedListTests
+    {
+        CircularLList circular = new CircularLList();
+        [TestMethod]
+        public void AddToEnd()
+        {            
+            circular.Add(5);
+            circular.Add(10);
+            circular.Add(15);            
+            circular.Add(20);
+            circular.Add(25);
+            Console.WriteLine("Expected List count = 5 and actual List count = "+circular.Count);
+            Assert.AreEqual(circular.Count, 5);
+        }
+
+        [TestMethod]
+        public void AddToFirst()
+        {   
+            circular.AddFirst(30);
+            circular.AddFirst(40);
+
+            int first = circular.GetFirstNode();
+            Assert.AreEqual(40, first);
+            Console.WriteLine("Expected: 40 and Actual: " + first);
+        }
+
+        [TestMethod]
+        public void DisplayAll()
+        {
+            circular.Display();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();           
+        }      
+
+        [TestMethod]
+        public void RemoveKey()
+        {
+            AddToEnd();
+            DisplayAll();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+
+            circular.Remove(25);
+            DisplayAll();
+            Assert.AreEqual(4, circular.Count);
+            Console.WriteLine("Expected: 4 and actual List Size: "+circular.Count);
+        }
+    }
+}
---
+++ CircularLinkedList/CircularLinkedList/CircularLList.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularLinkedList
+{
+    /*
+        [X] create a method to add a node to the list  
+        [X] create a method to display the contents of the list      
+        [X] create a method to Add to beginning of the list
+        [X] create a property to return the number of nodes in the list 
+        [X] create a method to Remove a node based on the given key
+    */
+    public class Node
+    {
+        public Node next = null;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+        public void AddToEnd(int data)
+        {
+            if (next == null)
+            {
+                next = new Node(data);
+
+            }
+            else
+            {
+                next.AddToEnd(data);
+            }
+        }
+        
+    }
+
+    public class CircularLList
+    {
+        Node head = null;
+        public int Count { get; set; }
+
+        public void Add(int data)
+        {
+            
+            Node current = head;
+            if (head == null)
+            {
+                head = new Node(data);
+                head.next = head;
+                Count += 1;
+            }
+            else
+            {
+                while (current.next != head)
+                {
+                    current = current.next;
+                }
+                
+                Node node = new Node(data);
+                current.next = node;
+                node.next = head;
+                Count += 1;
+           }
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            if (head != null)
+            {
+                do
+                {
+                    Console.Write("|" + current.data + "|->|");
+                    current = current.next;
+                }
+                while (current != head);                
+            }           
+        }
+
+        public void AddFirst(int data)
+        {
+            Node current = head;
+            if (head == null)
+            {
+                head = new Node(data);
+                head.next = head;
+                Count += 1;
+            }
+            else
+            {
+                Node node = new Node(data);
+                Count += 1;
+                while (current.next != head)
+                {
+                    current = current.next;                    
+                }
+                node.next = head;
+                current.next = node;
+                head = node;
+            }
+        }
+
+        public int GetFirstNode()
+        {
+            if (head != null)
+            {
+                return head.data;
+            }
+            else
+            {
+                Console.WriteLine("The list is empty");
+                return -1;
+            }
+        }
+
+        public int GetLastNode()
+        {
+            Node current = head;
+            if (current == null)
+            {                
+                Console.WriteLine("The list is empty");
+                return -1;
+            }
+            else
+            {
+                while (current.next != head)
+                {
+                    current = current.next;                    
+                }
+                return current.data;
+            }
+        }
+
+        public int Remove(int key)
+        {
+            Node current = head;
+            int removedData = 0;
+            Node previous = head;
+            if (head == null)
+            {
+                Console.WriteLine("The list is empty");
+            }
+            else
+            {
+                current=head.next;
+                while (current != head)
+                {
+                    if (current.data == key)
+                    {
+                        removedData = current.data;
+                        if (current.next == head)
+                        {
+                            current.next = null;
+                            previous.next = head;
+                            Count -= 1;
+                            return removedData;                            
+                        }
+                        else
+                        {
+                            previous.next = current.next;
+                        }
+                    }
+                    else
+                    {
+                        previous = current;
+                        current = current.next;                        
+                    }
+
+                }
+            }
+            return removedData;
+        }
+    }
+}
