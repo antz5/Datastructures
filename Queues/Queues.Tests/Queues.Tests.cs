a7e33ba6e8a83e8f4bc9bd83941fb67416e0e6bd
Ananth <antz5@users.noreply.github.com>
2016-12-15 11:19:32 +0530

    Implementation of Ordinary Queue Data structure with unit tests

---
+++ Queues/Queues.Tests/Queues.Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Queues.Tests
+{
+    [TestClass]
+    public class QueuesTests
+    {
+        Queues queue = new Queues(10);
+        [TestMethod]
+        public void Insert()
+        {
+            queue.Enqueue(5);
+            queue.Enqueue(10);
+            queue.Enqueue(15);
+            queue.Enqueue(20);
+            queue.Enqueue(25);
+            queue.Enqueue(30);
+            queue.Enqueue(35);
+            queue.Enqueue(40);
+            queue.Enqueue(45);
+            queue.Enqueue(50);
+
+            if (queue.Count > 0)
+            {
+                Assert.AreEqual(10, queue.Count);
+            }
+            Console.WriteLine("Expected count : 10 and Actual count: "+queue.Count);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+            if (queue.Count == 0)
+            {
+                Assert.AreEqual(0, queue.Count);
+            }
+            Console.WriteLine("Expected count : 0 and Actual count: " + queue.Count);
+        }
+
+        [TestMethod]
+        public void display()
+        {
+            Insert();
+            queue.Display();
+        }
+    }
+}
---
+++ Queues/Queues/Queues.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    /*
+        [X] create a method Enqueue to insert an item into the Queue 
+        [X] create a method Dequeue to delete an item from the Queue
+        [X] create a method IsEmpty to check if the queue is empty
+        [X] create a method IsFull to check if the queue is full
+        [X] create a property Count to get the number of elements in the queue
+    */
+    public class Queues
+    {
+        int front = -1;
+        int rear = -1;
+        int[] Queue;
+        public int Count { get; set; }
+
+        int size;
+        public Queues(int capacity)
+        {
+            size = capacity;
+            Queue = new int[size];
+        }
+
+        public void Enqueue(int data)
+        {
+            if (!IsFull())
+            {
+                Queue[++rear] = data;
+                Count += 1;
+            }
+            else
+                Console.WriteLine("Queue Overflow");
+        }
+
+        public int Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                Count -= 1;
+                return Queue[front++];
+               
+            }
+            return -1;
+        }
+
+        private bool IsEmpty()
+        {
+            if (rear == -1)
+                return true;
+            return false;
+        }
+
+        private bool IsFull()
+        {
+            if (front > rear)
+                return true;
+            else
+                return false;
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(Queue[i]);
+            }
+        }
+    }
+}
