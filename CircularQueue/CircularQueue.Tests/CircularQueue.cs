f39c113a720f6ce6d629d57ede14ff3fd0a0a478
Ananth <antz5@users.noreply.github.com>
2016-12-15 18:09:18 +0530

    Circular Queue Data structure with unit tests

---
+++ CircularQueue/CircularQueue.Tests/CircularQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircularQueue.Tests
+{
+    [TestClass]
+    public class CircularQueueTests
+    {
+        CircularQueue queue = new CircularQueue(10);
+
+        [TestMethod]
+        public void Insert()
+        {
+            queue.Enqueue(5);
+            queue.Enqueue(10);
+            queue.Enqueue(15);
+            queue.Enqueue(20);
+            queue.Enqueue(25);
+            queue.Enqueue(30);
+            queue.Enqueue(35);
+            queue.Enqueue(40);
+            queue.Enqueue(45);
+            queue.Enqueue(50);
+
+            if (queue.Count > 0)
+            {
+                Assert.AreEqual(10, queue.Count);
+            }
+            Console.WriteLine("Expected count : 10 and Actual count: " + queue.Count);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            Insert();
+            queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Dequeue();
+
+            if (queue.Count == 9)
+            {
+                Assert.AreEqual(9, queue.Count);
+            }
+            Console.WriteLine("Expected count : 9 and Actual count: " + queue.Count);
+            queue.Display();
+        }
+
+        [TestMethod]
+        public void display()
+        {
+            Insert();
+            queue.Display();
+        }
+    }
+}
---
+++ CircularQueue/CircularQueue/CircularQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CircularQueue
+{
+    public class CircularQueue
+    {
+        int front;
+        public int Count { get; set; }
+        int rear;
+        int size;
+        int[] queue;
+
+        public CircularQueue(int capacity)
+        {
+            size = capacity;
+            queue = new int[size];
+            Count = 0;
+            front = 0;
+            rear = -1;
+        }
+        private bool IsFull()
+        {
+            if (Count == size)
+                return true;
+            return false;
+        }
+
+        private bool IsEmpty()
+        {
+            if (Count == 0)
+                return true;
+            return false;
+        }
+
+        public void Enqueue(int data)
+        {
+            if (!IsFull())
+            {
+                rear = (rear + 1) % size;
+                queue[rear] = data;
+                Count += 1;
+            }
+            else
+                Console.WriteLine("Queue overflow");
+        }
+
+        public int Dequeue()
+        {
+            int data = -1;
+            if (!IsEmpty())
+            {
+                data = queue[front];
+                front = (front + 1) % size;
+                Count -= 1;
+            }
+            else
+                Console.WriteLine("Queue underflow");
+
+            return data;            
+        }
+
+        public void Display()
+        {
+            int i = front;
+            for (int j = 0; j < Count; j++)
+            {
+                Console.WriteLine(queue[i]);
+                i = (i + 1) % size;
+            }
+        }
+    }
+}
