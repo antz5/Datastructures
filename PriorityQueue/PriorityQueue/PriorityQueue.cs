bc105f184de6c59cf65c9d897a28ab8a614a3b44
Ananth <antz5@users.noreply.github.com>
2016-12-16 13:56:11 +0530

    Priority Queues and unit tests

---
+++ PriorityQueue/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue
+    {
+        int front = -1;
+        int rear = -1;
+        int[] Queue;
+        public int Count { get; set; }
+
+        int size;
+        public PriorityQueue(int capacity)
+        {
+            size = capacity;
+            Queue = new int[size];
+        }
+
+        public void Enqueue(int data)
+        {
+            if (!IsFull())
+            {
+                int j = rear;
+
+                while (j >= 0 && data < Queue[j])
+                {
+                    Queue[j + 1] = Queue[j];
+                    j--;
+                }
+                Queue[j + 1] = data;
+                rear += 1;
+                Count += 1;
+            }
+            else
+                Console.WriteLine("Queue Overflow");
+        }
+
+        public int Dequeue()
+        {
+            if (!IsEmpty())
+            {               
+                front += 1;
+                Count -= 1;
+                return Queue[front];
+
+            }
+            return -1;
+        }
+
+        private bool IsEmpty()
+        {
+            if (rear == -1)
+                return true;
+            return false;
+        }
+
+        private bool IsFull()
+        {
+            if (front > rear)
+                return true;
+            else
+                return false;
+        }
+
+        public void Display()
+        {
+            Console.Write("rear->");
+            for (int i = size-1; i >= 0; i--)
+            {
+                Console.Write(Queue[i]+"->");
+            }
+            Console.Write("front");
+        }
+    }
+}
---
+++ PriorityQueue/PriorityQueues.Tests/PriorityQueuesTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PriorityQueues.Tests
+{
+    [TestClass]
+    public class PriorityQueuesTests
+    {
+        PriorityQueue.PriorityQueue priority = new PriorityQueue.PriorityQueue(18);
+        [TestMethod]
+        public void Insert()
+        {
+            priority.Enqueue(10);
+            priority.Enqueue(11);
+            priority.Enqueue(18);
+            priority.Enqueue(1);
+            priority.Enqueue(20);
+            priority.Enqueue(3);
+            priority.Enqueue(7);
+            priority.Enqueue(21);
+            priority.Display();
+
+            Assert.AreEqual(8, priority.Count);
+            Console.WriteLine("Expected: 8 and actual: "+priority.Count);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            Insert();
+            Console.WriteLine();
+            priority.Dequeue();
+            priority.Display();
+            Console.WriteLine();
+            priority.Dequeue();
+            Console.WriteLine();
+            priority.Display();
+            Console.WriteLine();
+            priority.Enqueue(5);
+            priority.Display();
+
+            Assert.AreEqual(7, priority.Count);
+            Console.WriteLine("Expected: 7 and actual: " + priority.Count);
+        }
+
+    }
+}
