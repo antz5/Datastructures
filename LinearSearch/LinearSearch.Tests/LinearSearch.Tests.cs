4241af78da6a37a59fbc06773a119a8ccb3044c9
Ananth <antz5@users.noreply.github.com>
2016-12-18 13:59:40 +0530

    Linear Search Algorithm with unit tests

---
+++ LinearSearch/LinearSearch.Tests/LinearSearch.Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinearSearch.Tests
+{
+    [TestClass]
+    public class LinearSearchTests
+    {
+        LinearSearch search = new LinearSearch();
+        [TestMethod]
+        public void Search1()
+        {
+            int[] items = new int[] { 55, 25,36,78,2,49,156,79,125,146,789};
+            int key = 789;
+            int keyFoundAt = search.Search(items, key);
+            Assert.AreEqual(10, keyFoundAt);
+            Console.WriteLine("Expected: 10 and Actual: "+keyFoundAt);
+
+        }
+
+        [TestMethod]
+        public void Search2()
+        {
+            int[] items = new int[] { 55, 25, 36, 78, 2, 49, 156, 79, 125, 146, 789 };
+            int key = 146;
+            int keyFoundAt = search.Search(items, key);
+            Assert.AreEqual(9, keyFoundAt);
+            Console.WriteLine("Expected: 9 and Actual: " + keyFoundAt);
+
+        }
+
+        [TestMethod]
+        public void Search3()
+        {
+            int[] items = new int[] { 55, 25, 36, 78, 2, 49, 156, 79, 125, 146, 789 };
+            int key = 49;
+            int keyFoundAt = search.Search(items, key);
+            Assert.AreEqual(5, keyFoundAt);
+            Console.WriteLine("Expected: 5 and Actual: " + keyFoundAt);
+
+        }
+
+        [TestMethod]
+        public void Search4()
+        {
+            int[] items = new int[] { 55, 25, 36, 78, 2, 49, 156, 79, 125, 146, 789 };
+            int key = 1000;
+            int keyFoundAt = search.Search(items, key);
+            Assert.AreEqual(-1, keyFoundAt);
+            Console.WriteLine("Expected: -1 and Actual: " + keyFoundAt);
+
+        }
+        [TestMethod]
+        public void Search5()
+        {
+            int[] items = new int[] { 55, 25, 36, 78, 2, 49, 156, 79, 125, 146, 789 };
+            int key = 55;
+            int keyFoundAt = search.Search(items, key);
+            Assert.AreEqual(0, keyFoundAt);
+            Console.WriteLine("Expected: 0 and Actual: " + keyFoundAt);
+
+        }
+    }
+
+
+}
---
+++ LinearSearch/LinearSearch/LinearSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    public class LinearSearch
+    {
+        public int Search(int[] items, int key)
+        {
+            int position = -1;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == key)
+                {
+                    position = i;
+                    return position;
+                }
+            }
+            return position;
+        }
+    }
+}
+
