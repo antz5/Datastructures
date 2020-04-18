e9ae881057a96639861e67eaf8cb5c1d9f9df1bf
ananth <ananthsmurthy@gmail.com>
2020-04-18 18:34:14 +1000

    - Insert a Node to a SinglyLinkedList at the end using Iterative method

---
+++ HRProblems/LinkedLists/SinglyLinkedList/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+        }
+
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node)
+    {
+        while (node != null)
+        {
+            System.Console.WriteLine(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                System.Console.WriteLine();
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+    {
+        SinglyLinkedListNode current = head;
+        if (head == null)
+        {
+            return new SinglyLinkedListNode(data);
+        }        
+        else
+        {
+            while(current.next != null)
+            {                
+                current = current.next;            
+            }
+            
+            current.next = new SinglyLinkedListNode(data);
+        }
+        return head;
+    }
+
+    static void Main(string[] args)
+    {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++)
+        {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+            llist.head = llist_head;
+
+        }
+
+
+
+        PrintSinglyLinkedList(llist.head);
+        System.Console.WriteLine();
+
+
+    }
+}
