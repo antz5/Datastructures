88de3489cca66e2060bbeb8ee450917832c3b8c0
ananth <ananthsmurthy@gmail.com>
2020-04-19 14:51:36 +1000

    Insert a Node at a given position in a SinglyLinkedList

---
+++ HRProblems/LinkedLists/InsertNodeAtGivenPosition/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class SolutionInsertAtPosition {
+
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep) {
+        while (node != null) {
+            Console.Write(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                Console.Write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) 
+    {
+        int count = 0;
+        SinglyLinkedListNode current = head;
+        SinglyLinkedListNode prev = head;
+        if (head == null || position == 0)
+        {
+            return new SinglyLinkedListNode(data);
+        }        
+        else
+        {
+            while (current != null)
+            {                
+                if (count == position)
+                {
+                    SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+                    prev.next = newNode;
+                    newNode.next = current;
+                    return head;
+                }
+                prev = current;
+                current = current.next;
+                count++;
+            }
+        }
+        return head;
+    }
+
+    static void Main(string[] args) {
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            llist.InsertNode(llistItem);
+        }
+
+        int data = Convert.ToInt32(Console.ReadLine());
+
+        int position = Convert.ToInt32(Console.ReadLine());
+
+        SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+
+        PrintSinglyLinkedList(llist_head, " ");
+        
+    }
+}
