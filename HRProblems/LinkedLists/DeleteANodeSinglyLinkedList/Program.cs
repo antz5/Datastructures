e20e15524a6f5f2baf9f1c8630b35c4387b3410f
ananth <ananthsmurthy@gmail.com>
2020-04-19 18:39:38 +1000

    Given a head Print SinglyLinkedList in the reverse order

---
+++ HRProblems/LinkedLists/DeleteANodeSinglyLinkedList/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class SolutionDeleteANode {
+
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep) {
+        while (node != null) {
+            Console.Write(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                Console.Write(sep);
+            }
+        }
+    }
+
+    // Complete the deleteNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) 
+    {
+        SinglyLinkedListNode current = head;
+        SinglyLinkedListNode prev = head;
+
+        int count = 0;
+        if (head == null)
+        {
+            return head;
+        }
+        else
+        {
+            while (current != null)
+            {
+                if (count == position)
+                {
+                    SinglyLinkedListNode temp = current.next;
+                    prev.next = temp;
+                    current = null;
+                    return head;
+                }
+                prev = current;
+                current = current.next;
+                count++;
+            }
+        }
+        return head;
+    }
+
+    static void Main(string[] args) {
+        
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            llist.InsertNode(llistItem);
+        }
+
+        int position = Convert.ToInt32(Console.ReadLine());
+
+        SinglyLinkedListNode llist1 = deleteNode(llist.head, position);
+
+        PrintSinglyLinkedList(llist1, " ");
+    }
+}
