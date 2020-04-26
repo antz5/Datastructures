43b2d3a20443eddc9b9f9282f37d45865af5f4b6
ananth <ananthsmurthy@gmail.com>
2020-04-26 17:36:54 +1000

    BalancedBrackets using Stack

---
+++ HRProblems/Stacks/BalancedBrackets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        // Complete the isBalanced function below.
+        static string isBalanced(string s)
+        {
+            Stack<char> brackets = new Stack<char>();
+            
+            for(int i=0;i<s.Length;i++)
+            {
+                if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+                {
+                    brackets.Push(s[i]);
+                }
+                else
+                {
+                    if(brackets.Count == 0)
+                    {
+                        return "NO";
+                    }
+                    else 
+                    {
+                        char pop = brackets.Pop();
+                        if(s[i] == '}' && pop != '{')
+                        {
+                            return "NO";
+                        }
+                        else if (s[i] == ']' && pop != '[')
+                        {
+                            return "NO";
+                        }
+                        else if (s[i] == ')' && pop != '(')
+                        {
+                            return "NO";
+                        }
+                    }
+                }
+               
+            }
+            if (brackets.Count == 0)
+            {
+                return "YES";
+            }
+            return "NO";
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string s = Console.ReadLine();
+
+                string result = isBalanced(s);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
